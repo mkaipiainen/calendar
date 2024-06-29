548f674fcc7ffedfa2a33539d531732b3bf05601
Poltto <martti.kaipiainen@gmail.com>
2024-06-29 13:07:07 +0200

    Adjusted build pipeline

--- main.tf
+++ main.tf
@@ -1,7 +1,5 @@
-provider "aws" {
-  region     = var.aws_region
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+provider "digitalocean" {
+  token = var.digital_ocean_access_token
 }
 
 provider "cloudflare" {
@@ -9,245 +7,63 @@ provider "cloudflare" {
   api_key = var.cloudflare_api_key
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "172.17.0.0/16"
-}
-
-resource "aws_eip" "eip" {
-  domain = "vpc"
-}
-
-resource "aws_subnet" "public_a" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.17.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "eu-north-1a"
-}
-
-resource "aws_subnet" "public_b" {
-  vpc_id                  = aws_vpc.main.id
-  cidr_block              = "172.17.2.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "eu-north-1b"
-}
-
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main.id
-}
-
-resource "aws_route_table" "main" {
-  vpc_id = aws_vpc.main.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-}
-
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.public_a.id
-  route_table_id = aws_route_table.main.id
-}
-
-resource "aws_route_table_association" "b" {
-  subnet_id      = aws_subnet.public_b.id
-  route_table_id = aws_route_table.main.id
-}
-
-resource "aws_security_group" "web_sg" {
-  vpc_id = aws_vpc.main.id
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "web_sg"
-  }
-}
-
-resource "aws_lb" "nlb" {
-  name               = "my-nlb"
-  internal           = false
-  load_balancer_type = "network"
-  subnets            = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-}
-
-resource "aws_lb_target_group" "app" {
-  name        = "app-tg"
-  port        = 80
-  protocol    = "TCP"
-  vpc_id      = aws_vpc.main.id
-  target_type = "ip"
-  health_check {
-    path     = "/"
-    port     = "traffic-port"
-    protocol = "HTTP"
-  }
-}
-
-resource "aws_lb_listener" "nlb_listener" {
-  load_balancer_arn = aws_lb.nlb.arn
-  port              = 80
-  protocol          = "TCP"
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
-  }
-}
-
-resource "aws_lb_listener" "tls_listener" {
-  load_balancer_arn = aws_lb.nlb.arn
-  port              = 443
-  protocol          = "TLS"
-  ssl_policy        = "ELBSecurityPolicy-2016-08"
-  certificate_arn   = aws_acm_certificate.example.arn
-
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.app.arn
-  }
-
-  depends_on = [aws_acm_certificate.example]
-}
-
-resource "aws_acm_certificate" "example" {
-  domain_name       = var.domain_name
-  validation_method = "DNS"
-
-  tags = {
-    Name = "example-certificate"
-  }
-
-  lifecycle {
-    create_before_destroy = true
-  }
-}
-
-resource "cloudflare_record" "acm_validation" {
-  for_each = {
-    for dvo in aws_acm_certificate.example.domain_validation_options : dvo.domain_name => {
-      name    = dvo.resource_record_name
-      type    = dvo.resource_record_type
-      value   = dvo.resource_record_value
-      zone_id = var.cloudflare_zone_id
-    }
-  }
-
-  zone_id = each.value.zone_id
-  name    = each.value.name
-  type    = each.value.type
-  value   = each.value.value
-  ttl     = 300
-}
-
-resource "aws_iam_role" "ecs_task_execution" {
-  name = var.ecs_task_execution_role_name
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "ecs-tasks.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
-  role       = aws_iam_role.ecs_task_execution.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-}
-
-resource "aws_ecs_cluster" "main" {
-  name = "main-cluster"
-}
-
-resource "aws_ecs_task_definition" "web" {
-  family                   = "web-task"
-  network_mode             = "awsvpc"
-  requires_compatibilities = ["FARGATE"]
-  cpu                      = var.fargate_cpu
-  memory                   = var.fargate_memory
-  execution_role_arn       = aws_iam_role.ecs_task_execution.arn
-
-  container_definitions = jsonencode([
-    {
-      name      = "web-container"
-      image     = var.app_image
-      essential = true
-      portMappings = [
-        {
-          containerPort = 80
-          hostPort      = 80
-          protocol      = "tcp"
-        },
-        {
-          containerPort = 443
-          hostPort      = 443
-          protocol      = "tcp"
-        }
-      ]
-    }
-  ])
-}
-
-resource "aws_ecs_service" "web" {
-  name            = "web-service"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.web.arn
-  desired_count   = var.app_count
-  launch_type     = "FARGATE"
-
-  network_configuration {
-    subnets          = [aws_subnet.public_a.id, aws_subnet.public_b.id]
-    security_groups  = [aws_security_group.web_sg.id]
-    assign_public_ip = true
-  }
-
-  load_balancer {
-    target_group_arn = aws_lb_target_group.app.arn
-    container_name   = "web-container"
-    container_port   = var.app_port
-  }
-}
 
 resource "cloudflare_record" "storieslikegrapes" {
   zone_id = var.cloudflare_zone_id
-  name    = "storieslikegrapes"
-  value   = aws_eip.eip.public_ip
+  name    = var.domain_name
+  value   = digitalocean_reserved_ip.reserved_ip.ip_address
   type    = "A"
-  ttl     = 300
-  proxied = false
+  ttl     = 1
+  proxied = true
+}
+
+resource "digitalocean_reserved_ip" "reserved_ip" {
+  region = "fra1"
+}
+
+data "digitalocean_ssh_key" "ubuntu_ssh_key" {
+  name = "Desktop ubuntu"
+}
+
+data "digitalocean_ssh_key" "windows_ssh_key" {
+  name = "Desktop windows"
+}
+
+resource "digitalocean_droplet" "droplet" {
+  image  = "ubuntu-24-04-x64"
+  name   = "www-1"
+  region = "fra1"
+  size   = "s-1vcpu-512mb-10gb"
+  ssh_keys = [data.digitalocean_ssh_key.ubuntu_ssh_key.id, data.digitalocean_ssh_key.windows_ssh_key.id]
+
+  connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file("~/.ssh/id_rsa")
+    timeout = "2m"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt upgrade -y",
+      "curl -fsSL https://get.docker.com -o get-docker.sh",
+      "sudo sh ./get-docker.sh -y"
+    ]
+  }
+}
+
+resource "digitalocean_reserved_ip_assignment" "reserved_ip_assignment" {
+  ip_address = digitalocean_reserved_ip.reserved_ip.ip_address
+  droplet_id = digitalocean_droplet.droplet.id
 }
 
 terraform {
   required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
     }
     cloudflare = {
       source  = "cloudflare/cloudflare"
@@ -268,48 +84,11 @@ variable "cloudflare_zone_id" {
   description = "Cloudflare Zone ID"
 }
 
-variable "aws_access_key" {
-  description = "The IAM public access key"
-}
-
-variable "aws_secret_key" {
-  description = "IAM secret access key"
-}
-
-variable "aws_region" {
-  description = "The AWS region things are created in"
-}
-
-variable "ecs_task_execution_role_name" {
-  description = "ECS task execution role name"
-  default = "myEcsTaskExecutionRole"
-}
-
-variable "app_image" {
-  description = "Docker image to run in the ECS cluster"
-  default = "211125760731.dkr.ecr.eu-north-1.amazonaws.com/poltto/calendar-frontend:latest"
-}
-
-variable "app_port" {
-  description = "Port exposed by the docker image to redirect traffic to"
-  default = 443
-}
-
-variable "app_count" {
-  description = "Number of docker containers to run"
-  default = 1
-}
-
-variable "fargate_cpu" {
-  description = "Fargate instance CPU units to provision (1 vCPU = 1024 CPU units)"
-  default = "1024"
-}
-
-variable "fargate_memory" {
-  description = "Fargate instance memory to provision (in MiB)"
-  default = "2048"
-}
-
 variable "domain_name" {
   description = "Domain name for the SSL certificate"
 }
+
+variable "digital_ocean_access_token" {
+  description = "Digital Ocean Access Token"
+}
+
