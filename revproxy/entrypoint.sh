7fa5d7be2507c83887cf4a5d0ddf084663e028af
Poltto <martti.kaipiainen@gmail.com>
2024-07-06 15:48:27 +0200

    Adjusted build pipeline

--- revproxy/entrypoint.sh
+++ revproxy/entrypoint.sh
@@ -1,26 +1,9 @@
 #!/bin/sh
 
-# Debug logging to check if the secret file exists
-echo "Checking for secret file at /run/secrets/CLOUDFLARE_API_KEY"
+# Export the CLOUDFLARE_API_KEY environment variable from the secret
 if [ -f /run/secrets/CLOUDFLARE_API_KEY ]; then
-    export CLOUDFLARE_API_KEY=$(cat /run/secrets/CLOUDFLARE_API_KEY)
-    echo "Secret loaded into CLOUDFLARE_API_KEY"
-else
-    echo "Secret file not found!"
+  export CLOUDFLARE_API_KEY=$(cat /run/secrets/CLOUDFLARE_API_KEY)
 fi
 
-# Debug logging to check the content of the .env file
-if [ -f /etc/caddy/.env ]; then
-    echo "Loading environment variables from /etc/caddy/.env"
-    export $(grep -v '^#' /etc/caddy/.env | xargs)
-    cat /etc/caddy/.env
-else
-    echo "/etc/caddy/.env file not found!"
-fi
-
-# Start Caddy with debug logging to show environment variables
-echo "Starting Caddy with the following environment variables:"
-env
-
-# Start Caddy
-caddy run --config /etc/caddy/Caddyfile --adapter caddyfile --envfile /etc/caddy/.env
+# Execute the command passed to the entrypoint
+exec "$@"
