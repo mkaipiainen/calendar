64e12d3151962cb628e8f2869444f210679fd37e
Poltto <martti.kaipiainen@gmail.com>
2024-07-06 16:11:07 +0200

    Adjusted build pipeline

--- revproxy/entrypoint.sh
+++ revproxy/entrypoint.sh
@@ -2,7 +2,11 @@
 
 # Export the CLOUDFLARE_API_KEY environment variable from the secret
 if [ -f /run/secrets/CLOUDFLARE_API_KEY ]; then
+  echo "Secret file found, setting CLOUDFLARE_API_KEY"
   export CLOUDFLARE_API_KEY=$(cat /run/secrets/CLOUDFLARE_API_KEY)
+  echo "CLOUDFLARE_API_KEY is set to: $CLOUDFLARE_API_KEY"
+else
+  echo "Secret file not found, CLOUDFLARE_API_KEY not set"
 fi
 
 # Execute the command passed to the entrypoint
