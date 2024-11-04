a8d0e390176049064152d2504ec541a4bff86e66
Poltto <martti.kaipiainen@gmail.com>
2024-11-04 15:58:55 +0100

    Adjusted vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,15 +1,12 @@
-import { fileURLToPath, URL } from 'node:url'
+// vite.config.js
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vueDevTools from 'vite-plugin-vue-devtools';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   server: {
     host: '0.0.0.0',
     port: 80,
@@ -17,10 +14,19 @@ export default defineConfig({
       usePolling: true,
     },
     hmr: { clientPort: 8080 },
+    historyApiFallback: {
+      rewrites: [
+        // Exclude static assets from being rewritten to index.html
+        {
+          from: /^\/.*\.(js|css|png|jpg|jpeg|gif|svg|ico)$/,
+          to: (context) => context.parsedUrl.pathname,
+        },
+      ],
+    },
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
