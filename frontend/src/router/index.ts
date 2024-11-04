e3e6d0b0f2e04103f43113ebc34b45d7d8bcfba7
Poltto <martti.kaipiainen@gmail.com>
2024-11-04 15:54:17 +0100

    Adjusted routes and added a route guard integrated with auth0

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -1,24 +1,47 @@
 import { createRouter, createWebHistory } from 'vue-router';
+import { useAuth0 } from '@auth0/auth0-vue';
+import { watch } from 'vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'landing',
-      component: import('@/views/landing/LandingView.vue'),
+      redirect: 'plantminder',
     },
     {
       path: '/calendar',
       name: 'calendar',
-      component: import('@/views/calendar/CalendarView.vue'),
+      component: import('@/views/calendar/calendar.view.vue'),
     },
     {
       path: '/plantminder',
       name: 'plantminder',
-      component: import('@/views/plantminder/PlantMinder.vue'),
+      component: import('@/views/plantminder/plantminder.view.vue'),
     },
   ],
+});
+
+router.beforeEach(async (to, from) => {
+  const { isAuthenticated, isLoading, loginWithRedirect } = useAuth0();
+
+  if (isLoading.value) {
+    await new Promise<void>((resolve) => {
+      const unwatch = watch(isLoading, (newValue) => {
+        if (!newValue) {
+          unwatch();
+          resolve();
+        }
+      });
+    });
+  }
+
+  if (!isAuthenticated.value) {
+    await loginWithRedirect({ appState: { targetUrl: to.fullPath } });
+    return false; // Cancel the navigation
+  }
+
+  return true; // Proceed with the navigation
 });
 
 export default router;
